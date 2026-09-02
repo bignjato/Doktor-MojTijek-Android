3890d82e5ce049f5bb6e92b4d558a13f3a8eb6d5
bignjato <32342241+bignjato@users.noreply.github.com>
2026-09-02 21:27:51 +0200

    Add app/src/main/java/com/mojtijek/doktor/ui/theme/Color.kt

---
+++ app/src/main/java/com/mojtijek/doktor/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.mojtijek.doktor.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue700 = Color(0xFF1565C0)
+val Blue500 = Color(0xFF2F6BED)
+val Blue200 = Color(0xFF90CAF9)
+val Blue50  = Color(0xFFE3F2FD)
+
+val DarkBackground = Color(0xFF0D1117)
+val DarkSurface    = Color(0xFF161B22)
+val DarkOnSurface  = Color(0xFFC9D1D9)
